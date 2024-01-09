9e2623e5fc3022b82c300397dbd281921ed9ff6c
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-01-09 15:52:13 +0530

    sara

--- kvr/src/Components/Technical.jsx
+++ kvr/src/Components/Technical.jsx
@@ -1,4 +1,15 @@
 import React from 'react'
+import tool from '../Assets/Images/icons8-toolbox-64.png'
+import setting from '../Assets/Images/icons8-wired-network-48 (1).png'
+import operating from '../Assets/Images/icons8-windows-60.png'
+import jira from '../Assets/Images/icons8-jira-60.png'
+import git from '../Assets/Images/icons8-git-64.png'
+import database from '../Assets/Images/icons8-database-60.png'
+
+
+
+
+
 
 export default function Technical() {
     return (
@@ -9,19 +20,136 @@ export default function Technical() {
                     <div class="tf-heading tf-heading-style-02 mb-5 text-center">
                         <h2 class="tf-heading-title"  >Technical Skills</h2>
                     </div>
-                    <div className='row text-center'>
+                    <div className='row text-center g-3'>
                         <div className='col-xl-3 col-lg-4'>
-                            <div className='card custom-card p-5 '>
-                                <div className='w-50 mx-auto wrap-icon p-3 rounded-3 mb-2'>
-                                    <svg aria-hidden="true" class="e-font-icon-svg e-fab-git-alt" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M439.55 236.05L244 40.45a28.87 28.87 0 0 0-40.81 0l-40.66 40.63 51.52 51.52c27.06-9.14 52.68 16.77 43.39 43.68l49.66 49.66c34.23-11.8 61.18 31 35.47 56.69-26.49 26.49-70.21-2.87-56-37.34L240.22 199v121.85c25.3 12.54 22.26 41.85 9.08 55a34.34 34.34 0 0 1-48.55 0c-17.57-17.6-11.07-46.91 11.25-56v-123c-20.8-8.51-24.6-30.74-18.64-45L142.57 101 8.45 235.14a28.86 28.86 0 0 0 0 40.81l195.61 195.6a28.86 28.86 0 0 0 40.8 0l194.69-194.69a28.86 28.86 0 0 0 0-40.81z"></path></svg>
+                            <div className='card custom-card p-5 h-100 '>
+                                <div className='w-50 mx-auto wrap-icon p-3 rounded-3 mb-3'>
+                                    <img
+                                        src={tool}
+                                        // style={{backgroundColor:"red"}}
+                                        className="align-items-center  img-fluid"
+                                        alt=""
+                                    />
                                 </div>
-                                <p className='mb-2'>CI/CD</p>
-                                <div className='btn1 '>
+                                <p className='mb-3 heading3'>CI/CD Tools</p>
+                                <div className='btn1'>
                                     <button className='btn btn-primary px-4 py-2'>Read More</button>
                                 </div>
                             </div>
                         </div>
-                        
+                        <div className='col-xl-3 col-lg-4'>
+                            <div className='card custom-card p-5 h-100 '>
+                                <div className='w-50 mx-auto wrap-icon p-3 rounded-3 mb-3'>
+                                    <img
+                                        src={setting}
+                                        // style={{backgroundColor:"red"}}
+                                        className="align-items-center  img-fluid"
+                                        alt=""
+                                    />
+                                </div>
+                                <p className='mb-3 heading3'>Frame Work</p>
+                                <div className='btn1'>
+                                    <button className='btn btn-primary px-4 py-2'>Read More</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-xl-3 col-lg-4'>
+                            <div className='card custom-card p-5 h-100 '>
+                                <div className='w-50 mx-auto wrap-icon p-3 rounded-3 mb-3'>
+                                    <img
+                                        src={operating}
+                                        // style={{backgroundColor:"red"}}
+                                        className="align-items-center  img-fluid"
+                                        alt=""
+                                    />
+                                </div>
+                                <p className='mb-3 heading3'>Operating Systems</p>
+                                <div className='btn1'>
+                                    <button className='btn btn-primary px-4 py-2'>Read More</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-xl-3 col-lg-4'>
+                            <div className='card custom-card p-5 h-100 '>
+                                <div className='w-50 mx-auto wrap-icon p-3 rounded-3 mb-3'>
+                                    <img
+                                        src={jira}
+                                        // style={{backgroundColor:"red"}}
+                                        className="align-items-center  img-fluid"
+                                        alt=""
+                                    />
+                                </div>
+                                <p className='mb-3 heading3'>Defect Management Tool</p>
+                                <div className='btn1'>
+                                    <button className='btn btn-primary px-4 py-2'>Read More</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-xl-3 col-lg-4'>
+                            <div className='card custom-card p-5 h-100 '>
+                                <div className='w-50 mx-auto wrap-icon p-3 rounded-3 mb-3'>
+                                    <img
+                                        src={git}
+                                        // style={{backgroundColor:"red"}}
+                                        className="align-items-center  img-fluid"
+                                        alt=""
+                                    />
+                                </div>
+                                <p className='mb-3 heading3'>Version Controlling Tool</p>
+                                <div className='btn1'>
+                                    <button className='btn btn-primary px-4 py-2'>Read More</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-xl-3 col-lg-4'>
+                            <div className='card custom-card p-5 h-100 '>
+                                <div className='w-50 mx-auto wrap-icon p-3 rounded-3 mb-3'>
+                                    <img
+                                        src={database}
+                                        // style={{backgroundColor:"red"}}
+                                        className="align-items-center  img-fluid"
+                                        alt=""
+                                    />
+                                </div>
+                                <p className='mb-3 heading3'>Database Management</p>
+                                <div className='btn1'>
+                                    <button className='btn btn-primary px-4 py-2'>Read More</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-xl-3 col-lg-4'>
+                            <div className='card custom-card p-5 h-100 '>
+                                <div className='w-50 mx-auto wrap-icon p-3 rounded-3 mb-3'>
+                                    <img
+                                        src={setting}
+                                        // style={{backgroundColor:"red"}}
+                                        className="align-items-center  img-fluid"
+                                        alt=""
+                                    />
+                                </div>
+                                <p className='mb-3 heading3'>Web Services Testing</p>
+                                <div className='btn1'>
+                                    <button className='btn btn-primary px-4 py-2'>Read More</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-xl-3 col-lg-4'>
+                            <div className='card custom-card p-5 h-100 '>
+                                <div className='w-50 mx-auto wrap-icon p-3 rounded-3 mb-3'>
+                                    <img
+                                        src={database}
+                                        // style={{backgroundColor:"red"}}
+                                        className="align-items-center  img-fluid"
+                                        alt=""
+                                    />
+                                </div>
+                                <p className='mb-3 heading3'>Automaton Testing </p>
+                                <div className='btn1'>
+                                    <button className='btn btn-primary px-4 py-2'>Read More</button>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
