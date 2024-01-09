86d87f81ede5ffb3263e3aa7b615279a2e46f2d5
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-01-09 13:18:02 +0530

    new

--- kvr/src/App.js
+++ kvr/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Technical from './Components/Technical';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        {/* <Route path="/" element={<Home/>}/> */}
+        <Route path='/' element ={<Technical/>}/>
       </Routes>
       </BrowserRouter>
      
