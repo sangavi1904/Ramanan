f136a526ab35dc47bb72e7c9e37f9aca904ef92b
Jagan <jagannathan880@gmail.com>
2024-01-09 11:56:29 +0530

    new

--- kvr/src/App.js
+++ kvr/src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Details from './Components/Details';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        {/* <Route path="/" element={<Home/>}/> */}
+        <Route path="/" element={<Details/>}/>
+
       </Routes>
       </BrowserRouter>
      
---
+++ kvr/src/Components/Details.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Details() {
+  return (
+    <div className='container'>
+        <section>
+            
+        </section>
+    </div>
+  )
+}
+
+export default Details
