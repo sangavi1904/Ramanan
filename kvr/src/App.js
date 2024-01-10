63f6616bc0348454ac942c65033d665055db18b4
Jagan <jagannathan880@gmail.com>
2024-01-10 12:20:09 +0530

    sara

--- kvr/src/App.js
+++ kvr/src/App.js
@@ -1,15 +1,19 @@
 
 import './App.css';
+import './Jagan.css'
 
 // import Career from './Components/Career';
-import Nav from './Components/Nav';
+// import Nav from './Components/Nav';
 
 
 import './Diwa.css';
 
 // import Home from './Components/Home';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Technical from './Components/Technical';
+// import Technical from './Components/Technical';
+// import Home from './Components/Home';
+import Experience from './Components/Experience';
+
 
 function App() {
   return (
@@ -17,7 +21,9 @@ function App() {
       <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<Home/>}/> */}
-        <Route path="/" element={<Technical/>}/>
+        <Route path="/" element={<Experience/>}/>
+
+        {/* <Route path="/" element={<Technical/>}/> */}
       </Routes>
       </BrowserRouter>
      
